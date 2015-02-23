ac8e1c8974e960dd3ac2b37ea76856e82dc86b9a
Vicki Ball <vicki.ball@hypothetical.com.au>
2015-02-23 23:25:52 +1100

    Backport a fix from 4.2 to to 4.1, to avoid OUTPUT inserted.id issue with triggers

--- lib/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/active_record/connection_adapters/sqlserver_adapter.rb
@@ -44,8 +44,11 @@ module ActiveRecord
       cattr_accessor :native_text_database_type, :native_binary_database_type, :native_string_database_type,
                      :enable_default_unicode_types, :auto_connect, :cs_equality_operator,
                      :lowercase_schema_reflection, :auto_connect_duration, :showplan_option
+      
+      cattr_accessor :use_output_inserted, instance_accessor: false
 
       self.enable_default_unicode_types = true
+      self.use_output_inserted = true
 
       class BindSubstitution < Arel::Visitors::SQLServer # :nodoc:
         include Arel::Visitors::BindVisitor
